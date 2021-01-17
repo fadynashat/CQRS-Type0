a4665cec2dd0ddbd4dc1dc96e22948716fbf0ef0
fady nashat <fady.nashat.manhary@outlook.com>
2021-01-17 16:38:54 +0200

    start

---
+++ IC6.TutorialCQRS/Migrations/20190716150259_fii.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IC6.TutorialCQRS.Migrations
+{
+    public partial class fii : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
